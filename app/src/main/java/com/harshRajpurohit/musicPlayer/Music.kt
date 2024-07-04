7bf2820a913ee1c465c4a54b851cfaf7f9b543d4
IdeHarsh <ideharsh2023@gmail.com>
2024-07-04 17:20:27 +0530

    *Project Upgrade to Android Studio Koala 2024.1.1

--- app/src/main/java/com/harshRajpurohit/musicPlayer/Music.kt
+++ app/src/main/java/com/harshRajpurohit/musicPlayer/Music.kt
@@ -1,4 +1,4 @@
- package com.harshRajpurohit.musicPlayer
+package com.harshRajpurohit.musicPlayer
 
 import android.content.Context
 import android.graphics.Bitmap
@@ -11,24 +11,31 @@ import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
 
-data class Music(val id:String, val title:String, val album:String ,val artist:String, val duration: Long = 0, val path: String,
-val artUri:String)
+data class Music(
+    val id: String,
+    val title: String,
+    val album: String,
+    val artist: String,
+    val duration: Long = 0,
+    val path: String,
+    val artUri: String
+)
 
-class Playlist{
+class Playlist {
     lateinit var name: String
     lateinit var playlist: ArrayList<Music>
     lateinit var createdBy: String
     lateinit var createdOn: String
 }
 
-class MusicPlaylist{
+class MusicPlaylist {
     var ref: ArrayList<Playlist> = ArrayList()
 }
 
-fun formatDuration(duration: Long):String{
+fun formatDuration(duration: Long): String {
     val minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS)
     val seconds = (TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS) -
-            minutes*TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES))
+            minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES))
     return String.format("%02d:%02d", minutes, seconds)
 }
 
@@ -38,34 +45,34 @@ fun getImgArt(path: String): ByteArray? {
     return retriever.embeddedPicture
 }
 
-fun setSongPosition(increment: Boolean){
-    if(!PlayerActivity.repeat){
-        if(increment)
-        {
-            if(PlayerActivity.musicListPA.size - 1 == PlayerActivity.songPosition)
+fun setSongPosition(increment: Boolean) {
+    if (!PlayerActivity.repeat) {
+        if (increment) {
+            if (PlayerActivity.musicListPA.size - 1 == PlayerActivity.songPosition)
                 PlayerActivity.songPosition = 0
             else ++PlayerActivity.songPosition
-        }else{
-            if(0 == PlayerActivity.songPosition)
-                PlayerActivity.songPosition = PlayerActivity.musicListPA.size-1
+        } else {
+            if (0 == PlayerActivity.songPosition)
+                PlayerActivity.songPosition = PlayerActivity.musicListPA.size - 1
             else --PlayerActivity.songPosition
         }
     }
 }
 
-fun exitApplication(){
-    if(PlayerActivity.musicService != null){
+fun exitApplication() {
+    if (PlayerActivity.musicService != null) {
         PlayerActivity.musicService!!.audioManager.abandonAudioFocus(PlayerActivity.musicService)
         PlayerActivity.musicService!!.stopForeground(true)
         PlayerActivity.musicService!!.mediaPlayer!!.release()
-        PlayerActivity.musicService = null}
+        PlayerActivity.musicService = null
+    }
     exitProcess(1)
 }
 
-fun favouriteChecker(id: String): Int{
+fun favouriteChecker(id: String): Int {
     PlayerActivity.isFavourite = false
     FavouriteActivity.favouriteSongs.forEachIndexed { index, music ->
-        if(id == music.id){
+        if (id == music.id) {
             PlayerActivity.isFavourite = true
             return index
         }
@@ -73,39 +80,39 @@ fun favouriteChecker(id: String): Int{
     return -1
 }
 
-fun checkPlaylist(playlist: ArrayList<Music>): ArrayList<Music>{
+fun checkPlaylist(playlist: ArrayList<Music>): ArrayList<Music> {
     val indicesToRemove = mutableListOf<Int>()
 
     playlist.forEachIndexed { index, music ->
         if (!File(music.path).exists()) indicesToRemove.add(index)
     }
-    
+
     indicesToRemove.sortDescending()
     indicesToRemove.forEach { index -> playlist.removeAt(index) }
-    return  playlist
+    return playlist
 }
 
- fun setDialogBtnBackground(context: Context, dialog: AlertDialog){
-     //setting button text
-     dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE)?.setTextColor(
-         MaterialColors.getColor(context, R.attr.dialogTextColor, Color.WHITE)
-     )
-     dialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE)?.setTextColor(
-         MaterialColors.getColor(context, R.attr.dialogTextColor, Color.WHITE)
-     )
+fun setDialogBtnBackground(context: Context, dialog: AlertDialog) {
+    //setting button text
+    dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE)?.setTextColor(
+        MaterialColors.getColor(context, R.attr.dialogTextColor, Color.WHITE)
+    )
+    dialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE)?.setTextColor(
+        MaterialColors.getColor(context, R.attr.dialogTextColor, Color.WHITE)
+    )
 
-     //setting button background
-     dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE)?.setBackgroundColor(
-         MaterialColors.getColor(context, R.attr.dialogBtnBackground, Color.RED)
-     )
-     dialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE)?.setBackgroundColor(
-         MaterialColors.getColor(context, R.attr.dialogBtnBackground, Color.RED)
-     )
- }
+    //setting button background
+    dialog.getButton(android.app.AlertDialog.BUTTON_POSITIVE)?.setBackgroundColor(
+        MaterialColors.getColor(context, R.attr.dialogBtnBackground, Color.RED)
+    )
+    dialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE)?.setBackgroundColor(
+        MaterialColors.getColor(context, R.attr.dialogBtnBackground, Color.RED)
+    )
+}
 
- fun getMainColor(img: Bitmap): Int {
-     val newImg = Bitmap.createScaledBitmap(img, 1,1 , true)
-     val color = newImg.getPixel(0, 0)
-     newImg.recycle()
-     return color
- }
+fun getMainColor(img: Bitmap): Int {
+    val newImg = Bitmap.createScaledBitmap(img, 1, 1, true)
+    val color = newImg.getPixel(0, 0)
+    newImg.recycle()
+    return color
+}
